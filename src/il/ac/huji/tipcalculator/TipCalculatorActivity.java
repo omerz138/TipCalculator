b2b25fde7a04193ea626d80a7b40b27344f086b6
unknown <omer.zitzer@mail.huji.ac.il>
2014-02-25 17:06:44 +0200

    v1

--- src/il/ac/huji/tipcalculator/TipCalculatorActivity.java
+++ src/il/ac/huji/tipcalculator/TipCalculatorActivity.java
@@ -1,3 +1,6 @@
+/**
+ * @author omerz138
+ */
 package il.ac.huji.tipcalculator;
 
 import il.ac.huji.tipcalculator.R;
@@ -15,29 +18,29 @@ public class TipCalculatorActivity extends Activity  {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tip_calculator);
 		final Button calcBtt = (Button) findViewById(R.id.btnCalculate);
 		final EditText billAm = (EditText) findViewById(R.id.edtBillAmount);
 		final TextView txtTip = (TextView) findViewById(R.id.txtTipResult);
 		final CheckBox roundChk = (CheckBox) findViewById(R.id.chkRound);
+		
 		calcBtt.setOnClickListener(new OnClickListener()  {
 			double amount;
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
+				
 				amount = Double.parseDouble(billAm.getText().toString())*0.12;
 				if(roundChk.isChecked()){
 					txtTip.setText("Tip: $"+(int)Math.round(amount));
 				}
 				else{
-					txtTip.setText("Tip:$"+amount);
+					txtTip.setText("Tip: $"+amount);
 				}
 
 			}
 		});
 	}
-
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,6 +48,4 @@ public class TipCalculatorActivity extends Activity  {
 		getMenuInflater().inflate(R.menu.tip_calculator, menu);
 		return true;
 	}
-
-
 }
